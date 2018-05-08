1365bd8fe8aef07cfd93040db9980747a7a0cc5c
Eldho <eldho@live.in>
2018-05-08 14:13:00 +0530

    Added M version Check

--- app/src/main/java/com/example/eldho/easypermissionlibrarysample/MainActivity.java
+++ app/src/main/java/com/example/eldho/easypermissionlibrarysample/MainActivity.java
@@ -9,6 +9,7 @@ package com.example.eldho.easypermissionlibrarysample;
 
 import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -31,7 +32,9 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     }
 
     public void ClickMe(View view) {
-        reqCameraAndReadExternalStorage();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            reqCameraAndReadExternalStorage();
+        }
     }
 
     @AfterPermissionGranted(CAMERA_AND_STORAGE_PERMISSION_CODE)
